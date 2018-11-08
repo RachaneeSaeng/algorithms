408844dd66127ee458c004e62216b411ff2a9a61
apple@elephant.loans <apple@elephant.loans>
2018-11-08 20:04:00 +0700

    Add NTUC test

---
+++ Algorithms.Test/NTUCTest.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+using static Algorithms.Algorithms;
+
+namespace Algorithms.Test
+{
+    /// <summary>
+    ///This is a test class for AlgorithmsTest and is intended
+    ///to contain all AlgorithmsTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class NTUCTest
+    {
+        #region #1
+
+
+        [TestMethod()]
+        public void AllNegative()
+        {
+            var testData = new int[] { -1, -2 };
+
+            var s = new NTUC();
+            var result = s.Solution(testData);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod()]
+        public void AllNegative_Missing()
+        {
+            var testData = new int[] { -1, -2, 1, 3 };
+
+            var s = new NTUC();
+            var result = s.Solution(testData);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void NoMissing()
+        {
+            var testData = new int[] { 1, 2, 3 };
+
+            var s = new NTUC();
+            var result = s.Solution(testData);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod()]
+        public void HighValue()
+        {
+            var testData = new int[] { 1, 2, 3 };
+
+            var s = new NTUC();
+            var result = s.Solution(testData);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod()]
+        public void Mix()
+        {
+            var testData = new int[] { 1,1,2,5 };
+
+            var s = new NTUC();
+            var result = s.Solution(testData);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod()]
+        public void BigData()
+        {
+            var testDataSet = new List<int>();
+            for (int i = 0; i <= 100000; i++)
+            {
+                testDataSet.Add(i);
+            }
+
+            var s = new NTUC();
+            var result = s.Solution(testDataSet.ToArray());
+            Assert.AreEqual(100001, result);
+        }
+        #endregion
+
+    }
+}
---
+++ Algorithms/NTUC.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    public class NTUC
+    {
+        public int Solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            var positiveElemenets = Positive_MergeSort(A, 0, A.Length - 1);
+
+            int checking = 1;
+            for (int i = 0; i < positiveElemenets.Length; i++)
+            {
+                if (positiveElemenets[i] > checking)
+                {
+                    return checking;
+                }
+
+                if (positiveElemenets[i] == checking)
+                {
+                    checking++;
+                }
+            }
+            return checking;
+        }
+
+        private int[] Positive_MergeSort(int[] arr, int startIdx, int endIdx)
+        {
+            if (startIdx >= endIdx)
+            {
+                return new int[] { arr[startIdx]};
+            }
+
+            var midIdx = (startIdx + endIdx) / 2;
+            var leftArray = Positive_MergeSort(arr, startIdx, midIdx);
+            var rightArray = Positive_MergeSort(arr, midIdx + 1, endIdx);
+            return Merge(leftArray, rightArray);
+        }
+
+        private int[] Merge(int[] leftArray, int[] rightArray)
+        {
+            var result = new List<int>();
+
+            var leftIdx = 0;
+            var rightIdx = 0;
+            
+            while (leftIdx < leftArray.Length && rightIdx < rightArray.Length)
+            {
+                if (leftArray[leftIdx] <= 0)
+                {
+                    leftIdx++;
+                    continue;
+                }
+                if (rightArray[rightIdx] <= 0)
+                {
+                    rightIdx++;
+                    continue;
+                }
+
+                if (leftArray[leftIdx] <= rightArray[rightIdx])
+                {
+                    result.Add(leftArray[leftIdx]);
+                    leftIdx++;
+                }
+                else if (leftArray[leftIdx] > rightArray[rightIdx])
+                {
+                    result.Add(rightArray[rightIdx]);
+                    rightIdx++;
+                }
+            }
+
+            for (int i = leftIdx; i < leftArray.Length; i++)
+            {
+                result.Add(leftArray[i]);
+            }
+
+            for (int i = rightIdx; i < rightArray.Length; i++)
+            {
+                result.Add(rightArray[i]);
+            }
+            return result.ToArray();
+        }
+    }
+}
